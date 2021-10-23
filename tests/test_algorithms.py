34d8f5d303f72dc26d4beb9792952046a95a93f3
Kung-Ching Lin <kung.ching.lin@gmail.com>
2021-10-23 11:16:36 -0500

    Change the import line

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,11 +1,11 @@
-import toy_package.binary_search as bin_s
+import toy_package.bin_search as bs
 
 
 def test_bin_search_returns_index_when_exists() -> None:
     data = [0, 1, 3, 5]
     target = 3
 
-    outcome = bin_s(data, target)
+    outcome = bs.binary_search(data, target)
 
     assert outcome == 2
 
@@ -14,6 +14,6 @@ def test_bin_search_returns_default_when_not_exist() -> None:
     data = [1, 3, 6, 7]
     target = 2
 
-    outcome = bin_s(data, target)
+    outcome = bs.binary_search(data, target)
 
     assert outcome == -1
